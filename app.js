151360161480f181a4a5f92c708e658b3b5ed201
masudrana <masudranaacb@gmail.com>
2022-02-20 18:53:52 +0600

    using a filter & find object

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const numbers=[20,4,57,5,9,665,55];
 // const bigNumbers=numbers.filter(number=>number>50);
 const bigNumbers=numbers.find(number=>number>50);
-console.log(bigNumbers)
+// console.log(bigNumbers)
+
+const products=[
+    {name:'phone',color:'red',price:40},
+    {name:'phone',color:'balck',price:100},
+    {name:'phone',color:'balck',price:60},
+    {name:'phone',color:'green',price:50},
+    {name:'phone',color:'green',price:70},
+    {name:'phone',color:'red',price:120},
+]
+// const balckPhone=products.filter(product=>product.price<80);
+const balckPhone=products.find(product=>product.price==60);
+console.log(balckPhone)
